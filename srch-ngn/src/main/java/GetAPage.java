013b33fbd804b31d03e9cfd8637a90d82eb71291
geoPinkman <geo.pimkin@gmail.com>
2022-03-16 17:20:27 +1000

    geting full map

--- srch-ngn/src/main/java/GetAPage.java
+++ srch-ngn/src/main/java/GetAPage.java
@@ -9,43 +9,62 @@ import java.util.*;
 
 public class GetAPage {
 
-    public static String userAgent= "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15";
-    public static String referer = "https://www.google.com/";
-    public static String url = "https://www.google.com";
+    private static final String userAgent= "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15";
+    private static final String referer = "https://www.google.com/";
+    private static String url;// = "https://www.google.com";
 
+    public GetAPage(String url) {
+        GetAPage.url = url;
+    }
 
-    public static Set<String> getSetHrefs (String url) {
-       Set<String> hrefsSet = new HashSet<>();
-       try {
+    private Set<String> getSetHrefs (String url) {
+        Set<String> hrefsSet = new HashSet<>();
+        try {
             Document doc = Jsoup.connect(url)
                     .userAgent(userAgent)
                     .referrer(referer)
                     .get();
 
+            //Elements hrefs = doc.select("a[href^=/]");
             Elements hrefs = doc.select("a[href^=/]");
-            for (Element href : hrefs) {
-                String toCorrect = href.attr("href");
-                hrefsSet.add(toCorrect);
-            }
+            hrefs.forEach(line -> hrefsSet.add(line.attr("href")));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         return hrefsSet;
     }
-    @NotNull
-    public static Set<String> getSiteMap (@NotNull Set<String> set) {
-        Set<String> resultSet = new HashSet<>(getSetHrefs(url));
-        set.forEach(line -> getSetHrefs(url + line).forEach(resultSet::add));
-        return resultSet;
+    private String getContext(String href) {
+        StringBuilder context = new StringBuilder();
+        try {
+            Document doc = Jsoup.connect(href)
+                    .userAgent(userAgent)
+                    .referrer(referer)
+                    .get();
+            Elements page = doc.getAllElements();
+            for (Element text : page) {
+                context.append(text);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return context.toString().replaceAll("\n", "");
     }
-
+    @NotNull
+    public Map<String, String> getSiteMap (@NotNull Set<String> set) {
+        Map<String, String> map = new Hashtable<>();
+        Set<String> resultSet = new HashSet<>();
+        set.forEach(line -> resultSet.addAll(getSetHrefs(url + line)));
+        resultSet.forEach(line -> {
+            if (!line.contains(".")) {
+                map.put(line, "");
+            }
+        });
+        return map;
+    }
     public static void main(String[] args) {
-
         long start = System.currentTimeMillis();
-        System.out.println(getSetHrefs(url).size());
-        getSetHrefs(url).stream().sorted().forEach(System.out :: println);
-        getSiteMap(getSetHrefs(url)).stream().sorted().forEach(System.out::println);
-
+        GetAPage page = new GetAPage("https://www.svetlovka.ru");
+        page.getSiteMap(page.getSetHrefs(url)).keySet().forEach(System.out::println);
         System.out.println(System.currentTimeMillis() - start + " ms");
     }
 }
--- srch-ngn/src/test/java/Test.java
+++ srch-ngn/src/test/java/Test.java
@@ -1,6 +1,5 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -11,25 +10,19 @@ public class Test {
 
     public static String userAgent= "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15";
     public static String referer = "https://www.google.com/";
-    public static String url = "https://www.svetlovka.ru/";
+    public static String url = "https://www.svetlovka.ru";
 
     public static Set<String> getSetHrefs (String url) {
         Set<String> hrefsSet = new HashSet<>();
+
         try {
             Document doc = Jsoup.connect(url)
                     .userAgent(userAgent)
                     .referrer(referer)
                     .get();
-
             Elements hrefs = doc.select("a[href^=/]");
-            for (Element href : hrefs) {
-                System.out.println(href.attr("href"));
-                //System.out.println(href.attr("href"));
-//                String toCorrect = href.attr("href");
-//                if (toCorrect.length() > 1 & toCorrect.charAt(0) == '/') {
-//                    hrefsSet.add(toCorrect);
-//                }
-            }
+            hrefs.forEach(line -> hrefsSet.add(line.attr("href")));
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -39,7 +32,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        getSetHrefs(url).forEach(System.out :: println);
+        getSetHrefs(url).stream().sorted().forEach(System.out::println);
 
 
     }
